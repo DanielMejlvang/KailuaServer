c61d996ca425d3aec0dedcdc13eb9df236075b1c
Daniel <58211711+caerKea@users.noreply.github.com>
2020-04-30 12:38:32 +0200

    Tilføjet Zip.doesExist()

--- Kailua/src/main/java/com/example/demo/Repository/ZipRepository.java
+++ Kailua/src/main/java/com/example/demo/Repository/ZipRepository.java
@@ -1,8 +1,11 @@
 package com.example.demo.Repository;
 
 import com.example.demo.Model.Zip;
+import com.sun.rowset.internal.Row;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,14 +14,20 @@ public class ZipRepository {
     JdbcTemplate template;
 
     public Boolean doesExist(Zip zip){
-        return null;
+        String sql = "SELECT * FROM zips WHERE zip = ?";
+        boolean result = false;
+        int count = template.queryForObject(sql, new Object[] { zip.getZip() }, Integer.class);
+        if(count > 0){
+            result = true;
+        }
+        return result;
     }
 
     public Zip addZip(Zip zip){
         return null;
     }
 
-    public Zip editZip(){
+    public Zip editZip(String zipID, Zip newZip){
         return null;
     }
 }
--- Kailua/src/main/java/com/example/demo/Service/ZipService.java
+++ Kailua/src/main/java/com/example/demo/Service/ZipService.java
@@ -1,9 +1,25 @@
 package com.example.demo.Service;
 
+import com.example.demo.Model.Zip;
+import com.example.demo.Repository.ZipRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ZipService {
+    @Autowired
+    ZipRepository zr;
 
+    public Boolean doesExist(Zip zip){
+        return zr.doesExist(zip);
+    }
+
+    public Zip addZip(Zip zip){
+        return zr.addZip(zip);
+    }
+
+    public Zip editZip(String zipID, Zip newZip){
+        return zr.editZip(zipID, newZip);
+    }
 
 }
